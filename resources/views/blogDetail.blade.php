7a93fff8d031c99583103af90c79e2d0855c0603
aep <aepsaprul18@gmail.com>
2022-12-17 10:20:08 +0700

    update blog detail

--- resources/views/blogDetail.blade.php
+++ resources/views/blogDetail.blade.php
@@ -12,7 +12,10 @@
   
   <div class="af-blog-detail-content">
     <div class="af-blog-detail-container">
-      <img src="{{ asset('http://localhost/abata_hrd/public/compro/blog/' . $blog->gambar) }}" alt="">
+      {{-- dev --}}
+      <img src="{{ asset('http://localhost/abata_hrd/public/compro/blog/' . $blog->gambar) }}" alt="blog">
+      {{-- prod --}}
+      {{-- <img src="{{ asset('https://hcm.abata-printing.com/public/compro/blog/' . $blog->gambar) }}" alt="blog"> --}}
       <div class="af-blog-detail-deskripsi">{!! $blog->deskripsi !!}</div>
       <hr>
       <div class="af-blog-detail-tanggal">Terbit: {{ $blog->created_at }}</div>
